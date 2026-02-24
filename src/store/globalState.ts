16f729ce8d2d0c56c1e8dfb985944ec26856d664
Mehrob007 <mehrobsanginov2006@gmail.com>
2026-02-24 10:23:52 +0500

    feat: Introduce core utilities for API client with token refresh, global state management, form data types, and global styling.

---
+++ src/store/globalState.ts
@@ -0,0 +1,19 @@
+import { create } from "zustand";
+
+export const globalState = create<{
+  modal: boolean;
+  setModal: (value: boolean) => void;
+  toast: { active: boolean, type: "error" | "success" };
+  setToast: (value: { active: boolean, type: "error" | "success" }) => void;
+  openModalKey: string;
+  setOpenModalKey: (key: string) => void;
+  checkKeyModal: (key: string) => boolean;
+}>((set, get) => ({
+  modal: false,
+  setModal: (value: boolean) => set(() => ({ modal: value })),
+  toast: { active: true, type: "error" },
+  setToast: (value: { active: boolean, type: "error" | "success" }) => set(() => ({ toast: value })),
+  openModalKey: "",
+  setOpenModalKey: (key: string) => set(() => ({ openModalKey: key })),
+  checkKeyModal: (key: string) => get().openModalKey === key,
+}));

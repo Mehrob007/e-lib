2ea41d70e284701dffa77418fcb9b6b8fe2898c6
Mehrob007 <mehrobsanginov2006@gmail.com>
2026-03-03 22:37:46 +0500

    feat: convert login page to client component, replace `next/image` with SVG component, and integrate `useFormStore` for input state.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,21 +1,27 @@
-import loginIcon from "@/../public/icons/login-icon.svg";
+"use client";
+
+import LoginIcon from "@/../public/icons/login-icon.svg";
 import Input from "@/components/elements/input/Input";
-import Image from "next/image";
+import { useFormStore } from "@/hooks/useFormStore";
 
 export default function Login() {
-
-    
+  const { data, errors, setData } = useFormStore();
 
   return (
     <div className="login__page">
       <main>
         <div className="login__content">
           <div>
-            <Image src={loginIcon} alt="login-icon" />
+            <LoginIcon aria-label="login-icon" />
           </div>
 
           <div className="login__form">
-            <Input  value="" />
+            <Input
+              id="login"
+              value={String(data.login ?? "")}
+              onChange={(e) => setData("login", e)}
+              errors={errors}
+            />
           </div>
         </div>
       </main>

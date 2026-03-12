a043c27b42d03ef50bfc495e470a44f0bf627242
Mehrob007 <mehrobsanginov2006@gmail.com>
2026-03-12 09:32:01 +0500

    feat: Update admin menu link to `/admin/super-admin`.

--- src/components/ui/LeftMenu/LeftMenu.tsx
+++ src/components/ui/LeftMenu/LeftMenu.tsx
@@ -2,7 +2,7 @@ import IconMenuAdmin from "@/../public/icons/icon-menu-admin.svg";
 import Link from "next/link";
 
 export default function LeftMenu({ open }: { open: boolean }) {
-  const menuList = [{ title: "Админы", icon: <IconMenuAdmin />, href: "/admin" }];
+  const menuList = [{ title: "Админы", icon: <IconMenuAdmin />, href: "/admin/super-admin" }];
 
   return (
     <main className={`left__menu ${open ? "open__left_menu" : ""}`}>

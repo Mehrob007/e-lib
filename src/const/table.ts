ac4b0c6a1237966d1c875c8a8af060d9ba7ec681
Mehrob007 <mehrobsanginov2006@gmail.com>
2026-03-12 15:43:48 +0500

    feat: Implement a new reusable table component with dynamic headers, item rendering, password visibility toggle, and row actions.

---
+++ src/const/table.ts
@@ -0,0 +1,18 @@
+export const HeaderTableUser = [
+  {
+    name: "Имя",
+    key: "username",
+  },
+  {
+    name: "Логин",
+    key: "phone_number",
+  },
+  {
+    name: "Пароль",
+    key: "password",
+  },
+//   {
+//     name: "Управление",
+//     key: "specialty_letter",
+//   },
+];

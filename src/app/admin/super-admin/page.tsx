a8152492bbdff9d920aef116cc8fa354f5792471
Mehrob007 <mehrobsanginov2006@gmail.com>
2026-03-12 15:42:46 +0500

    feat: Introduce reusable Table and Modal UI components, enhancing the admin section with new catalog management and updated navigation.

--- src/app/admin/super-admin/page.tsx
+++ src/app/admin/super-admin/page.tsx
@@ -1,18 +1,69 @@
-import Link from "next/link";
+"use client";
+// import { getSpecialtysREQ } from "@/api/specialty";
+import Button from "@/components/elements/button/Button";
+// import DashboardLayout from "@/components/ui/sidebar/DashboardLayout";
+import Input from "@/components/elements/input/Input";
+import { HeaderTableUser } from "@/const/table";
+import { ItemT } from "@/types/table";
+import { useCallback, useEffect, useState } from "react";
+import TableItems from "@/modules/table/table/TableItems";
+import Modal from "@/components/ui/modal/Modal";
+import { getUsersREQ } from "@/api/user";
+// import "./specialty.css";
 
-export default function AdminPage() {
+export default function Specialty() {
+  const [page, setPage] = useState(1);
+  const [data, setData] = useState<ItemT[] | null>(null);
+  const [search, setSearch] = useState("");
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const fetchData = useCallback(async () => {
+    try {
+      const res = await getUsersREQ();
+      return res as unknown as ItemT[];
+    } catch (e) {
+      console.error(e);
+      return null;
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchData().then((d) => {
+      if (d) setData(d);
+    });
+  }, [page, fetchData, search]);
+
+  useEffect(() => {
+    if (!isModalOpen) {
+      fetchData().then((d) => {
+        if (d) setData(d);
+      });
+    }
+  }, [isModalOpen, fetchData]);
+
   return (
-    <div className="p-8">
-      <h1 className="text-3xl font-bold text-red-600 mb-4">Админ-панель</h1>
-      <p className="text-lg">
-        Эта страница доступна только пользователям с ролью ADMIN.
-      </p>
-      <Link
-        href="/"
-        className="mt-4 inline-block text-blue-500 hover:underline"
-      >
-        Вернуться на главную
-      </Link>
-    </div>
+    <>
+      <div className="specialty__content">
+        <TableItems
+          styleHeader={{ gridTemplateColumns: "repeat(var(--cols, 3), 1fr)" }}
+          styleTable={{ gridTemplateColumns: "repeat(var(--cols, 3), 1fr)" }}
+          header={HeaderTableUser}
+          items={{
+            data: [
+              { username: "test", phone_number: "test", password: "test" },
+            ],
+            keys: HeaderTableUser.map((e) => e.key),
+          }}
+          setPage={setPage}
+          page={page}
+          openModalAdd={() => setIsModalOpen(true)}
+          personIcon={true}
+        />
+      </div>
+
+      {isModalOpen && (
+        <Modal onClose={() => setIsModalOpen(false)} onSuccess={fetchData} />
+      )}
+    </>
   );
 }

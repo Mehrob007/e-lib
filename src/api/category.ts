1b82ea598abf651296023459f9999ef0118c41ac
Mehrob007 <mehrobsanginov2006@gmail.com>
2026-03-13 11:43:20 +0500

    feat: Redesign admin panel navigation and tables, add category management, and introduce new statistics and content pages.

--- src/api/category.ts
+++ src/api/category.ts
@@ -1,18 +1,18 @@
 import { dataT } from "@/types/useFormStore";
 import apiClient from "@/utils/apiClient";
 
-export const getUsersREQ = async () => {
+export const getCategorysREQ = async (id?: string) => {
   try {
-    const res = await apiClient("user");
+    const res = await apiClient("categorys" + (id ? "/" + id : ""));
     return res.data;
   } catch (e) {
     console.error(e);
   }
 };
 
-export const postUserREQ = async (data: dataT) => {
+export const postCatalogREQ = async (data: dataT) => {
   try {
-    const res = await apiClient.post("user", data);
+    const res = await apiClient.post("categorys", data);
     return res.data;
   } catch (e) {
     console.error(e);

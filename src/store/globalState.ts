9fed40d108dbdb80bc5ef2a959f49abb4894777a
Mehrob007 <mehrobsanginov2006@gmail.com>
2026-03-13 22:46:06 +0500

    feat: Introduce category and elements content styles, add `folderLine` type, and enhance table component props for interactive elements.

--- src/store/globalState.ts
+++ src/store/globalState.ts
@@ -3,8 +3,8 @@ import { create } from "zustand";
 export const globalState = create<{
   modal: boolean;
   setModal: (value: boolean) => void;
-  toast: { active: boolean, type: "error" | "success" };
-  setToast: (value: { active: boolean, type: "error" | "success" }) => void;
+  toast: { active: boolean; type: "error" | "success" };
+  setToast: (value: { active: boolean; type: "error" | "success" }) => void;
   openModalKey: string;
   setOpenModalKey: (key: string) => void;
   checkKeyModal: (key: string) => boolean;
@@ -12,7 +12,8 @@ export const globalState = create<{
   modal: false,
   setModal: (value: boolean) => set(() => ({ modal: value })),
   toast: { active: true, type: "error" },
-  setToast: (value: { active: boolean, type: "error" | "success" }) => set(() => ({ toast: value })),
+  setToast: (value: { active: boolean; type: "error" | "success" }) =>
+    set(() => ({ toast: value })),
   openModalKey: "",
   setOpenModalKey: (key: string) => set(() => ({ openModalKey: key })),
   checkKeyModal: (key: string) => get().openModalKey === key,

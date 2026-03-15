c9b4ed744f03a1b962f171542ae79f208179a8d6
Mehrob007 <mehrobsanginov2006@gmail.com>
2026-03-15 21:55:48 +0500

    refactor: Standardize loading state management using `finally` blocks in data fetching and modals, simplify modal `onSuccess` callbacks, and correct table item loading display.

--- src/api/user.ts
+++ src/api/user.ts
@@ -4,7 +4,7 @@ import apiClient from "@/utils/apiClient";
 export const getUsersREQ = async () => {
   try {
     const res = await apiClient("user");
-    return res.data;
+    return res?.data;
   } catch (e) {
     console.error(e);
   }
@@ -13,7 +13,7 @@ export const getUsersREQ = async () => {
 export const postUserREQ = async (data: dataT) => {
   try {
     const res = await apiClient.post("user", data);
-    return res.data;
+    return res?.data;
   } catch (e) {
     console.error(e);
   }

5025ee5adf824bf129a0ec6efb8f02e124fe0caa
Mehrob007 <mehrobsanginov2006@gmail.com>
2026-03-18 04:58:44 +0500

    feat: Introduce `SelectT` interface and custom select input styles, alongside a refactor of the elements page header.

--- src/types/input.ts
+++ src/types/input.ts
@@ -9,3 +9,15 @@ export interface InputT {
   errors?: { [key: string]: string };
   style?: React.CSSProperties;
 }
+
+export interface SelectT {
+  value: string;
+  onChange: (e: string) => void;
+  options: { value: string; label: string }[];
+  title?: string;
+  placeholder?: string;
+  className?: string;
+  id: string;
+  errors?: { [key: string]: string };
+  style?: React.CSSProperties;
+}

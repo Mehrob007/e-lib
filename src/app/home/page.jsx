c9af3a8267580ea7789586923228a8afa6e595d2
Mehrob007 <mehrobsanginov2006@gmail.com>
2026-03-21 22:31:00 +0500

    feat: Implement initial home page with a header component including a search input.

---
+++ src/app/home/page.jsx
@@ -0,0 +1,9 @@
+import HeaderHome from "../../components/ui/header/HeaderHome";
+
+export default function Page() {
+  return (
+    <div className="home-page">
+      <HeaderHome />
+    </div>
+  );
+}

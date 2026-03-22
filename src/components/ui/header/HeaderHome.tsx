90038d644524397d223378f183f881c785eb7dbe
Mehrob007 <mehrobsanginov2006@gmail.com>
2026-03-22 22:21:30 +0500

    feat: Add a language selection dropdown to the HeaderHome component, update its layout and styling, and extend the Select type definition.

--- src/components/ui/header/HeaderHome.tsx
+++ src/components/ui/header/HeaderHome.tsx
@@ -1,13 +1,32 @@
+"use client";
 import LogoHeaderHome from "@/../public/icons/logo-header-home.svg";
 import SearchInput from "@/components/elements/input/SearchInput";
+import Select from "@/components/elements/select/Select";
 export default function HeaderHome() {
   return (
     <div className="header-home">
       <LogoHeaderHome />
-      <span>
-        <h1>КИТОБХОНА</h1>
-      </span>
-      <SearchInput />
+      <nav>
+        <span>
+          <h1>КИТОБХОНА</h1>
+        </span>
+        <SearchInput onSearch={() => {}} />
+      </nav>
+      <Select
+        labelStyle={{ minWidth: "200px", maxWidth: "200px" }}
+        inputStyle={{ width: "auto", margin: 0 }}
+        // key={"lang_select"}
+        id={"lang_select"}
+        // title={""}
+        value={""}
+        onChange={() => {}}
+        options={[
+          { label: "Тоҷикӣ", value: "tj" },
+          { label: "Русский", value: "ru" },
+          { label: "English", value: "en" },
+        ]}
+        placeholder={""}
+      />
     </div>
   );
 }

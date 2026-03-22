c4b47573c372618189b0aaf6588a0338e6cc9c6f
Mehrob007 <mehrobsanginov2006@gmail.com>
2026-03-22 22:12:57 +0500

    feat: Add language selector to header, enhance Select component with styling props, and update home page layout and styling.

--- src/components/elements/input/SearchInput.tsx
+++ src/components/elements/input/SearchInput.tsx
@@ -1,5 +1,6 @@
-"use client"
+"use client";
 import { useState } from "react";
+import { IoSearchOutline } from "react-icons/io5";
 
 interface Props {
   onSearch: (v: string) => void;
@@ -8,9 +9,16 @@ interface Props {
 export default function SearchInput({ onSearch }: Props) {
   const [v, setV] = useState("");
   return (
-    <div>
-      <input type="text" value={v} onChange={(e) => setV(e.target.value)} />
-      <button onClick={() => onSearch(v)}>Ҷустуҷӯ</button>
+    <div className="search-input">
+      <input
+        placeholder="Ҷустуҷӯ аз рӯи ном ё муаллиф"
+        type="text"
+        value={v}
+        onChange={(e) => setV(e.target.value)}
+      />
+      <button onClick={() => onSearch(v)}>
+        <IoSearchOutline /> Ҷустуҷӯ
+      </button>
     </div>
   );
 }

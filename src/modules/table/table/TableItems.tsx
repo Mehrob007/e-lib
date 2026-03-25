3ac58b7bc57b762a391f5283dc808c740b0acc26
Mehrob007 <mehrobsanginov2006@gmail.com>
2026-03-25 18:57:05 +0500

    refactor: Standardize table item IDs to string, pass deleteItem prop to TableItem, and refine global styling for table navigation.

--- src/modules/table/table/TableItems.tsx
+++ src/modules/table/table/TableItems.tsx
@@ -47,6 +47,7 @@ export default function TableItems({
                 key={i}
                 keys={items.keys}
                 data={e}
+                deleteItem={deleteItem}
               />
             ))
           ) : (

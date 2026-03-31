30331262639bfbe404bc36dd82c047d46a2013c0
Mehrob007 <mehrobsanginov2006@gmail.com>
2026-03-31 23:37:43 +0500

    feat: implement guest token retrieval and flatten element details for table display

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,11 +1,20 @@
 import apiClient from "@/utils/apiClient";
 
 export const postAuthREQ = async (data: {
-  phone_snumber: string;
+  phone_number: string;
   password: string;
 }) => {
   try {
-    const res = await apiClient.post("/auth", data);
+    const res = await apiClient.post("/admin/login", {}, { params: data });
+    return res.data;
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+export const getTokenGuestREQ = async () => {
+  try {
+    const res = await apiClient("/get-guest-token");
     return res.data;
   } catch (e) {
     console.error(e);

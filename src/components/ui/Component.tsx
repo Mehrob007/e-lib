1f070b76e2a53619ffb845321b6705077aea923d
Mehrob007 <mehrobsanginov2006@gmail.com>
2026-03-31 23:38:48 +0500

    feat: implement new modal and card UI components with associated styling and API integration

--- src/components/ui/Component.tsx
+++ src/components/ui/Component.tsx
@@ -3,15 +3,19 @@ import React, { useState } from "react";
 import LeftMenu from "./leftMenu/LeftMenu";
 import Header from "./header/Header";
 import { usePathname } from "next/navigation";
+import GetTopkenGuest from "../elements/trigger/GetTopkenGuest";
 
 export default function Component({ children }: { children: React.ReactNode }) {
   const [openMenu, setOpenMenu] = useState(false);
   const pathName = usePathname();
+
+  const isAdmin = pathName?.includes("admin");
   return (
     <main className="component__main">
-      {pathName?.includes("admin") && <Header />}
-      <div>
-        {pathName?.includes("admin") && <LeftMenu open={openMenu} />}
+      {!isAdmin && <GetTopkenGuest />}
+      {isAdmin && <Header />}
+      <div style={{ paddingRight: !isAdmin ? "0" : "" }}>
+        {isAdmin && <LeftMenu open={openMenu} />}
         {children}
       </div>
     </main>

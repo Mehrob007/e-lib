006cd438fc5458ee372f777cf8bf512566ef7dcf
Mehrob007 <mehrobsanginov2006@gmail.com>
2026-04-01 22:23:48 +0500

    feat: implement nested catalog navigation components and update apiClient with cookie-based token management

--- src/components/ui/header/HeaderHome.tsx
+++ src/components/ui/header/HeaderHome.tsx
@@ -2,12 +2,21 @@
 import LogoHeaderHome from "@/../public/icons/logo-header-home.svg";
 import SearchInput from "@/components/elements/input/SearchInput";
 import Select from "@/components/elements/select/Select";
+import { usePathname, useRouter } from "next/navigation";
 export default function HeaderHome() {
+  const pathName = usePathname();
+  const router = useRouter();
+
+  console.log("pathName", pathName);
+
   return (
     <div className="header-home">
       <LogoHeaderHome />
       <nav>
-        <span>
+        <span
+          className={pathName === "/home" ? "active" : ""}
+          onClick={() => router?.push("/home")}
+        >
           <h1>КИТОБХОНА</h1>
         </span>
         <SearchInput onSearch={() => {}} />

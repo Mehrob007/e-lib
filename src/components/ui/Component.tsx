1a439fba2fe7d10a9d24b60335ebc56a246d17ba
Mehrob007 <mehrobsanginov2006@gmail.com>
2026-04-03 15:07:52 +0500

    feat: implement useBranding hook to dynamically update site logo, favicon, and title across components

--- src/components/ui/Component.tsx
+++ src/components/ui/Component.tsx
@@ -4,16 +4,18 @@ import LeftMenu from "./leftMenu/LeftMenu";
 import Header from "./header/Header";
 import { usePathname } from "next/navigation";
 import GetTopkenGuest from "../elements/trigger/GetTopkenGuest";
+import { useBranding } from "@/hooks/useBranding";
 
 export default function Component({ children }: { children: React.ReactNode }) {
-  const [openMenu, setOpenMenu] = useState(false);
+  const [openMenu] = useState(false);
   const pathName = usePathname();
+  const branding = useBranding();
 
   const isAdmin = pathName?.includes("admin");
   return (
     <main className="component__main">
       {!isAdmin && <GetTopkenGuest />}
-      {isAdmin && <Header />}
+      {isAdmin && <Header logo={branding?.logo as string} />}
       <div style={{ paddingRight: !isAdmin ? "0" : "" }}>
         {isAdmin && <LeftMenu open={openMenu} />}
         {children}
